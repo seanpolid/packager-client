d8e503b8394fd482bc57559759c439e44d62a5e7
seanpolid <seanpolidori0@gmail.com>
2024-02-10 21:28:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PackagerClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+    }
+}
---
+++ dtos/PackageInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackagerClient.dtos
+{
+    internal class PackageInfoDTO
+    {
+        public string ContainerName { get; set; }
+        public string RepoPath { get; set; }
+    }
+}
