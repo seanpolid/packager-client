d177daaf826b0c0e3dbadad0572555d21875259a
seanpolid <seanpolidori0@gmail.com>
2024-02-10 22:19:30 -0500

    Made client

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace PackagerClient
+﻿using PackagerClient.clients;
+
+namespace PackagerClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Directory.GetCurrentDirectory());
+            string? server = Environment.GetEnvironmentVariable("PACKAGE_SERVER");
+            if (server == null)
+            {
+                Console.WriteLine("Could not identify the packager server. Please ensure the environment variable PACKAGE_SERVER is set.");
+                return;
+            }
+
+            string? containerName = Environment.GetEnvironmentVariable("CONTAINER_NAME");
+            if (containerName == null)
+            {
+                Console.WriteLine("Could not identify the name of the container. Please ensure the environment variable CONTAINER_NAME is set.");
+                return;
+            }
+
+            string path = Directory.GetCurrentDirectory();
+            Client.SendPackageInfo(server, containerName, path);
         }
     }
 }
---
+++ clients/Client.cs
@@ -0,0 +1,42 @@
+﻿using PackagerClient.dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PackagerClient.clients
+{
+    internal class Client
+    {
+        private static readonly HttpClient httpClient = new();
+        private static readonly JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        public static void SendPackageInfo(string server, string containerName, string path)
+        {
+            PackageInfoDTO packageInfo = new()
+            {
+                ContainerName = containerName,
+                RepoPath = path
+            };
+
+            HttpRequestMessage request = new()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("http://" + server + "/package"),
+                Content = JsonContent.Create<PackageInfoDTO>(packageInfo, null, jsonSerializerOptions)
+            };
+
+            HttpResponseMessage response = httpClient.Send(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Could not successfully communicate with package server.");
+            }
+        }
+    }
+}
